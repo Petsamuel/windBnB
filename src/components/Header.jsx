d43ced2e45ee16e638cd5381431bd89c37c71b24
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-01-02 16:24:48 +0100

    fix list-items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   const [location, setLocation] = useState([]);
-  const [dropdownlist, setdropdownlist]= useState();
-  const [active, setactive] = useState(true);
+  const [dropdownlist, setdropdownlist] = useState();
+  const [active, setactive] = useState(false);
 
   const droplocation = [
     "Helsinki, Finland",
@@ -18,17 +18,18 @@ function Header() {
   ];
   useEffect(() => {
     locations.map((index) => {
-      setLocation(index.city +"," + index.country);
-
+      setLocation(index.city + "," + index.country);
     });
   }, [active]);
-function dropdownflag(){
-  setdropdownlist("hello")
-}
+  function dropdownflag() {
+    setdropdownlist("hello");
+    setactive(!active);
+  }
+
   return (
     <React.Fragment>
       <nav>
-        <div className="brand-container">
+        <div className={active ? "hidden" : "brand-container"}>
           <div className="brand-logo">
             <img src={logo} alt="windBnb-logo" />
           </div>
@@ -36,19 +37,45 @@ function dropdownflag(){
 
         <div
           className={
-            active ? "search-container input-group" : "search-container-active"
+            active
+              ? "search-container-active input-group"
+              : "search-container input-group"
           }
         >
           <div className="location-search">
-            <span onClick={(e)=>{dropdownflag()}}>{dropdownlist ? dropdownlist : "sdsdsd"}</span>
-            <ul className="location-list">
-              {droplocation.map((value, key)=>{
-                 <li key={key}>{value}dfdfdf</li>
-              })}
-             
+            <span
+              onClick={(e) => {
+                dropdownflag();
+              }}
+              className={
+                active ? "location-display-active" : "location-display"
+              }
+            >
+              {dropdownlist ? location : location}
+            </span>
+            <ul className="location-list ">
+              {droplocation.map((index, key) => (
+                <li
+                  key={key}
+                  className="location-list-item"
+                  onClick={() => {
+                    setLocation(index)
+                  }}
+                >
+                  {index}
+                </li>
+              ))}
             </ul>
           </div>
-          <div className="Guest-search"></div>
+
+          <div className="Guest-search">
+            Guest
+            <span>Add guess</span>
+            <div className="guest-list">
+              <div>Adults</div>
+              <div>Children</div>
+            </div>
+          </div>
           <div className="search-button">
             {" "}
             <i>
