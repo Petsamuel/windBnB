8e3a8105ad3fc91b55ada1d20c14b05ccf951c2e
petsamuel <petsamuel4@gmail.com>
2023-07-08 18:00:21 +0100

    change

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,7 +10,8 @@ export const Search = () => {
     const [ActiveLocation] = useGlobalState("locationLists");
     const [total] = useGlobalState("total")
     const [location, setlocation] = useState()
-    const locationState = () => {
+    const [menuSwitch, setMenuSwitch] = useState(true)
+    const Switcher = (e) => {
         setSearchState({ ...searchState, searchActive: !searchState.searchActive })
     }
 
@@ -19,27 +20,28 @@ export const Search = () => {
             <div className={'bg-white  my-5  '}>
                 <div className=''>
                     <ul className={searchState.searchActive ? 'w-full flex gap-4 shadow-md  align-start items-center rounded-xl' : "flex gap-4 shadow-md px-5  items-center rounded-xl"}>
-                        <li className={searchState.searchActive ? 'flex flex-col w-[25em] p-2 border-[1px] rounded-xl border-gray-700' : 'flex flex-col'} onClick={locationState}><a href="#" className='cursor-pointer '>
+                        <li className={searchState.searchActive ? 'flex flex-col w-[25em] p-2 border-[1px] rounded-xl border-gray-700' : 'flex flex-col'} onClick={()=>{Switcher}}><a href="#" className='cursor-pointer '>
                             <div className={searchState.searchActive ? ' text-md' : 'hidden'}>Location </div>
                             {searchState.searchActive ? (location ? location : "Helsinki, Finland") : "Helsinki, Finland"}</a></li>
                         <li className='w-[1px] h-8 bg-gray-300 py-7'></li>
-                        <li className={searchState.searchActive ? 'flex flex-col w-[25em] p-2' : 'flex flex-col'} ><a href="#" className='cursor-pointer '>
+                        <li className={searchState.searchActive ? 'flex flex-col w-[25em] p-2' : 'flex flex-col'} onClick={()=>{Switcher('')}}><a href="#" className='cursor-pointer '>
                             <div className={searchState.searchActive ? ' text-md' : 'hidden'}>Guess </div>
                             {total != 0 ? total : "Add"} Guests</a></li>
                         <li className='w-[1px] h-8 bg-gray-300 py-7'></li>
                         <li className={searchState.searchActive ? 'flex flex-col w-[25em] p-2' : 'flex flex-col'}><a className='cursor-pointer flex items-center justify-center'>{searchState.searchActive ? <Button /> : <FontAwesomeIcon icon={faMagnifyingGlass} />} </a></li>
 
                     </ul>
-                    <div className='flex justify-between  w-[65%] mx-4'>
-                        <ul className={searchState.searchActive ? 'hidden lg:block mt-10' : "hidden"}>
-                            {ActiveLocation.map((value, index) => (
-                                <li key={index} className="cursor-pointer flex gap-4 items-center my-4" onClick={() => { setlocation(value.name) }}><FontAwesomeIcon icon={faLocationDot} />{value.name}</li>
-                            ))}
+                    <div className='flex justify-between  items-start  mx-4 align-middle relative w-[58%]'>
+                        <ul className={searchState.searchActive ? 'hidden  mt-10 lg:flex flex-col items-start ' : "hidden"}>
+
+                            {menuSwitch ? ActiveLocation.map((value, index) => (
+                                <li key={index} className="cursor-pointer flex gap-4 items-center my-4 align-middle" onClick={() => { setlocation(value.name) }}><FontAwesomeIcon icon={faLocationDot} />{value.name}</li>
+                            )) : ""}
                         </ul>
-                        <div className={searchState.searchActive ? 'mt-10' : 'hidden'}>
+                        <div className={searchState.searchActive ? 'mt-10  w-80 lg:flex items-start hidden' : 'hidden'} >
                             <ButtonCounter />
                         </div>
-                        <div></div>
+
                     </div>
 
 
